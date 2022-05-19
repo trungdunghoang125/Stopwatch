5fe576396ccd6cd9295764c90a1d08e9b72b24bc
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-19 11:54:07 +0700

    My implementation of function start, pause and reset the stopwatch

--- app/src/main/java/com/trungdunghoang125/stopwatch/MainActivity.kt
+++ app/src/main/java/com/trungdunghoang125/stopwatch/MainActivity.kt
@@ -2,10 +2,62 @@ package com.trungdunghoang125.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById(R.id.stopwatch)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                pauseButton.text = "Resume"
+                running = false
+            }
+            else {
+                pauseButton.text = "Pause"
+                stopwatch.start()
+                setBaseTime()
+                running = true
+            }
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+            running = false
+            pauseButton.text = "Pause"
+            stopwatch.stop()
+        }
     }
+
+    fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
 }
