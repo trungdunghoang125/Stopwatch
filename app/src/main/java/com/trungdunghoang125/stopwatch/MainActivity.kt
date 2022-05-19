df31c840f3d408e581416d78a390d89a1ed91ef6
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-19 10:06:47 +0700

    Initial commit

---
+++ app/src/main/java/com/trungdunghoang125/stopwatch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trungdunghoang125.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
